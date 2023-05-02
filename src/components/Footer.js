0423d7a6bd40d8f9c12eb0c6b598227f5ace92ab
Tatsumi Suzuki <hej@ahadigital.se>
2023-05-02 08:26:00 +0200

    Add godkänd för f-skatt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,28 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer>
-				<div className="row">
-					<div className="col col-3 margin-sm">
-						<p className="h3">E-post:</p>
-						<p>hej@ahadigital.se</p>
-					</div>
-					<div className="col col-3 margin-sm">
-						<p className="h3">Telefon:</p>
-						<p>+46 (0) 735-00 30 34</p>
-					</div>
-					<div className="col col-3 margin-sm">
-						<p className="h3">Organisationsnummer:</p>
-						<p>559017-3802</p>
-					</div>
-					<div className="col col-3 margin-sm">
-						<p className="h3">Säte:</p>
-						<p>Stockholm</p>
-					</div>
+	return (
+		<footer>
+			<div className="row">
+				<div className="col col-3 margin-sm">
+					<p className="h3">E-post:</p>
+					<p>hej@ahadigital.se</p>
 				</div>
-			</footer>
-  )
+				<div className="col col-3 margin-sm">
+					<p className="h3">Telefon:</p>
+					<p>+46 (0) 735-00 30 34</p>
+				</div>
+				<div className="col col-3 margin-sm">
+					<p className="h3">Organisationsnummer:</p>
+					<p>559017-3802</p>
+					<p>Godkänd för F-skatt</p>
+				</div>
+				<div className="col col-3 margin-sm">
+					<p className="h3">Säte:</p>
+					<p>Stockholm</p>
+				</div>
+			</div>
+		</footer>
+	)
 }
 export default Footer
