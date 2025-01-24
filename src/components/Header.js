ecbeb9cbdaab5bbb82efbcd3b9690c5b4af260be
Cecilia Fredriksson <c.fredrikssons@gmail.com>
2025-01-24 21:45:53 +0100

    adress

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,8 +12,8 @@ const Header = ({
   return (
     <header className="row margin-lg">
       <div className="col col-6 margin-md">
-        <h3>Hyresavi</h3>
-        <h4>Smallworld Nordic Sweden AB</h4>
+        <h3>Bokföring</h3>
+        <h4>AF bergarbeten AB</h4>
       </div>
       <div className="col col-6">
         <h2 className="h2 margin-df">Fakturanummer: {invoiceNumber}</h2>
@@ -30,9 +30,9 @@ const Header = ({
             <h3 className="h3">Er referens:</h3>
           </div>
           <div className="col col-8">
-            <p style={{ whiteSpace: 'pre-line' }} className="margin-sm">{'Cecilia Fredriksson'}</p>
-            <p style={{ whiteSpace: 'pre-line' }}>{'Smallworld Nordic Sweden AB'}</p>
-            <p style={{ whiteSpace: 'pre-line' }}>{'S:t Göransgatan 82, 112 38 Stockholm'}</p>
+            <p style={{ whiteSpace: 'pre-line' }} className="margin-sm">{'Anders Fredriksson'}</p>
+            <p style={{ whiteSpace: 'pre-line' }}>{'AF bergarbeten AB'}</p>
+            <p style={{ whiteSpace: 'pre-line' }}>{'Steninge backe 5, 195 58 Märsta'}</p>
           </div>
         </div>
         <div className="row margin-df">
@@ -40,7 +40,7 @@ const Header = ({
             <h3 className="h3">Vår referens:</h3>
           </div>
           <div className="col col-8">
-            <p>{'Cecilia Fredriksson/Tatsumi Suzuki'}</p>
+            <p>{'Cecilia Fredriksson'}</p>
           </div>
         </div>
         <div className="row">
